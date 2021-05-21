9ae9a65d18c8ee1f240136ada8276544ee3cd6c7
sukanya-mankala <66584027+sukanya-mankala@users.noreply.github.com>
2021-05-21 11:05:18 +0530

    jenkins

---
+++ src/jenkninsTest/Hello.java
@@ -0,0 +1,13 @@
+package jenkninsTest;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Sukanya");
+		
+
+	}
+
+}
